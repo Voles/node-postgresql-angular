2d95559a461734af4d770bc10ab7e88786b9ca7e
Niels Dequeker <niels.dequeker@gmail.com>
2015-07-16 22:26:46 +0200

    Improve connection pool for PostgreSQL

--- api/database/postgresql.js
+++ api/database/postgresql.js
@@ -1,7 +1,14 @@
 var pg              = require('pg'),
   connectionString  = 'postgres://localhost:5432/newfront';
 
-var client = new pg.Client(connectionString);
-client.connect();
-
-exports.client = client;
+// via http://stackoverflow.com/a/19282657/363448
+module.exports = {
+  query: function (text, values, cb) {
+    pg.connect(connectionString, function (err, client, done) {
+      client.query(text, values, function (err, result) {
+        done();
+        cb(err, result);
+      })
+    });
+  }
+};
--- api/routes/events.js
+++ api/routes/events.js
@@ -2,19 +2,9 @@ var express = require('express');
 var db = require('../database/postgresql.js');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res) {
-  var results = [];
-
-  var query = db.client.query("SELECT * FROM events ORDER BY id ASC");
-
-  // Stream results back one row at a time
-  query.on('row', function(row) {
-    results.push(row);
-  });
-
-  query.on('end', function() {
-    return res.json(results);
+  db.query('SELECT * FROM events ORDER BY id ASC', [], function (err, result) {
+    return res.json(result);
   });
 });
 
